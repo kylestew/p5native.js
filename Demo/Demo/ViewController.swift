906ebb17bdbb5deba76375e6b679dddeee559727
Kyle Stewart <kylestew@gmail.com>
2015-12-31 21:50:31 -0800

    Setup framework project with demo embedded for development

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+import p5native
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hello().world()
+    }
+
+}
+
---
+++ p5native/p5native.h
@@ -0,0 +1,19 @@
+//
+//  p5native.h
+//  p5native
+//
+//  Created by Kyle Stewart on 12/31/15.
+//  Copyright © 2015 Kyle Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for p5native.
+FOUNDATION_EXPORT double p5nativeVersionNumber;
+
+//! Project version string for p5native.
+FOUNDATION_EXPORT const unsigned char p5nativeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <p5native/PublicHeader.h>
+
+
